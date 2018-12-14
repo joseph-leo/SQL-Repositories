002f0aae0276e2a328f6109825503e958f144d5f
joseph-leo <43657372+joseph-leo@users.noreply.github.com>
2018-12-13 17:50:09 -0800

    Update SQL_Lab.sql

--- SQL_Lab.sql
+++ SQL_Lab.sql
@@ -1,25 +1,20 @@
-SELECT * FROM Production.Product
+EXEC dbo.product_search @subCat = 'Helmets', @cat = 'Accessories';
 
-SELECT * FROM Production.ProductCategory
-
-SELECT * FROM Production.Product
-
-
-DECLARE @subCat NVARCHAR(50) = 'LIGHTS';
-DECLARE @cat NVARCHAR(50) = 'Accessories';
-
-SELECT PC.Name AS Category,
-       PSC.Name AS Subcategory,
-       PM.Name AS Model,
-       P.Name AS Product
-FROM Production.Product AS P
-FULL OUTER JOIN
-       Production.ProductModel AS PM
-       ON PM.ProductModelID = P.ProductModelID
-FULL OUTER JOIN
-       Production.ProductSubcategory AS PSC
-       ON PSC.ProductSubcategoryID = P.ProductSubcategoryID
-INNER JOIN
-       Production.ProductCategory AS PC
-       ON PC.ProductCategoryID = PSC.ProductCategoryID
-WHERE PC.Name = @cat AND PSC.Name = @subCat;
+ CREATE PROC dbo.product_search @subCat NVARCHAR(50), @cat NVARCHAR(50)
+ AS
+	SELECT PC.Name AS Category,
+		   PSC.Name AS Subcategory,
+		   PM.Name AS Model,
+		   P.Name AS Product
+	FROM Production.Product AS P
+	FULL OUTER JOIN
+		   Production.ProductModel AS PM
+		   ON PM.ProductModelID = P.ProductModelID
+	FULL OUTER JOIN
+		   Production.ProductSubcategory AS PSC
+		   ON PSC.ProductSubcategoryID = P.ProductSubcategoryID
+	INNER JOIN
+		   Production.ProductCategory AS PC
+		   ON PC.ProductCategoryID = PSC.ProductCategoryID
+	WHERE PC.Name = @cat AND PSC.Name = @subCat;
+GO
